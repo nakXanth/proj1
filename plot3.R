7d3c10f43b2f121e6ddaab2c90eb397ec1e8dc10
Nick Keune <nickkeune@gmail.com>
2017-08-24 14:49:05 -0400

    new stuffds
    new stuffds

--- plot3.R
+++ plot3.R
@@ -7,26 +7,28 @@ hpcGood<-hpc[!goodData,]
 
 dateB<-hpcGood$Date == "2/1/2007" | hpcGood$Date == "2/2/2007"
 hpcDate <- hpc[dateB,]
+FullDateTime<-strptime(paste0(hpcDate$Date," ",hpcDate$Time), format="%m/%d/%Y %H:%M:%S")
+
 
 #1
 pdf("plot1.png")
-hist(as.numeric(hpcDate$Global_active_power)/1000, breaks=10)
+hist(as.numeric(hpcDate$Global_active_power)/1000, breaks=10,main="Global Active Power",xlab="Global Active Power (kilowatts)" )
 dev.copy(png,'plot1.png')
 dev.off();
 
 
 #2
 pdf("plot2.png")
-plot(strptime(paste0(hpcDate$Date," ",hpcDate$Time), format="%m/%d/%Y %H:%M:%S"), as.numeric(hpcDate$Global_active_power), type="l" )
+plot(strptime(paste0(hpcDate$Date," ",hpcDate$Time), format="%m/%d/%Y %H:%M:%S"), as.numeric(hpcDate$Global_active_power), type="l" ,ylab="Global Active Power (kilowatts)",xlab="")
 dev.copy(png,'plot2.png')
 dev.off();
 
 
 #3
 pdf("plot3.png")
-with(hpcDate, plot(FullDateTime,Sub_metering_3,type="l",col="blue") )
-with(hpcDate, points(FullDateTime,Sub_metering_1,type="p",col="yellow") )
-with(hpcDate, points(FullDateTime,Sub_metering_2,type="l",col="red") )
+with(hpcDate, plot(FullDateTime,Sub_metering_3,type="l",col="blue",xlab="",ylab="Energy sub metering") )
+with(hpcDate, points(FullDateTime,Sub_metering_1,type="p",col="black"),xlab="",label="Sub_metering_1" )
+with(hpcDate, points(FullDateTime,Sub_metering_2,type="l",col="red"),xlab="" )
 dev.copy(png,'plot3.png')
 dev.off();
 
@@ -34,14 +36,14 @@ dev.off();
 pdf("plot4.png")
 par(mfrow = c(2,2))
 with(hpcDate,{
-	plot(strptime(paste0(hpcDate$Date," ",hpcDate$Time), format="%m/%d/%Y %H:%M:%S"), as.numeric(hpcDate$Global_active_power), type="l" )
-	plot(strptime(paste0(hpcDate$Date," ",hpcDate$Time), format="%m/%d/%Y %H:%M:%S"), as.numeric(hpcDate$Voltage), type="l" )
+	plot(strptime(paste0(hpcDate$Date," ",hpcDate$Time), format="%m/%d/%Y %H:%M:%S"), as.numeric(hpcDate$Global_active_power), type="l",ylab="Global Active Power",xlab="" )
+	plot(strptime(paste0(hpcDate$Date," ",hpcDate$Time), format="%m/%d/%Y %H:%M:%S"), as.numeric(hpcDate$Voltage), type="l" ,ylab="Voltage",xlab="")
 
-	plot(FullDateTime,Sub_metering_3,type="l",col="blue")
-	points(FullDateTime,Sub_metering_1,type="p",col="yellow")
+	plot(FullDateTime,Sub_metering_3,type="l",col="blue",ylab="Energy sub metering",xlab="")
+	points(FullDateTime,Sub_metering_1,type="p",col="black")
 	points(FullDateTime,Sub_metering_2,type="l",col="red")
 
-	plot(strptime(paste0(hpcDate$Date," ",hpcDate$Time), format="%m/%d/%Y %H:%M:%S"), as.numeric(hpcDate$Global_reactive_power), type="l" )
+	plot(strptime(paste0(hpcDate$Date," ",hpcDate$Time), format="%m/%d/%Y %H:%M:%S"), as.numeric(hpcDate$Global_reactive_power), type="l",ylab="Global_reactive_power",xlab="" )
 })
 dev.copy(png,'plot4.png')
 
